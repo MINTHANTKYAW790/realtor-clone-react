fc191f6c8f83bb02dce0546791a7e54a67cfe03c
Min Thant <kthura397@gmail.com>
2023-12-10 16:19:30 +0630

    create header and link between routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 function App() {
     return (
         <Router>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router";
+export default function Header() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    function pathMathRouth(Route) {
+        if (Route === location.pathname) {
+            console.log(location.pathname);
+            return true;
+        }
+    }
+
+    return (
+        <div>
+            <header className="sticky z-10">
+                <div className="flex justify-between items-center  px-20 border-b-0 shadow-md h-12 ">
+                    <img
+                        src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+                        alt="realtor logo"
+                        className="h-5 cursor-pointer"
+                        onClick={() => navigate("/")}
+                    />
+                    <ul className="flex justify-center items-center space-x-10 space-y-0 h-5/5 ">
+                        <li
+                            className={`cursor-pointer border-b-2 border-b-transparent text-gray "${
+                                pathMathRouth("/") &&
+                                "text-black border-b-red-950 font-bold"
+                            }`}
+                            onClick={() => navigate("/")}
+                        >
+                            Home
+                        </li>
+                        <li
+                            className={`cursor-pointer border-b-2 border-b-transparent text-gray"${
+                                pathMathRouth("/offers") &&
+                                "text-black border-b-red-950 font-bold"
+                            }`}
+                            onClick={() => navigate("/offers")}
+                        >
+                            Offers
+                        </li>
+                        <li
+                            className={`cursor-pointer border-b-2 border-b-transparent text-gray "${
+                                pathMathRouth("/sign-in") &&
+                                "text-black border-b-red-950 font-bold"
+                            }`}
+                            onClick={() => navigate("/sign-in")}
+                        >
+                            SignIn
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    );
+}
